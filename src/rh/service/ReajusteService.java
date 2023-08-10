dcd5cc673fe47bc1d45f1c03b98ada314c447a78
carreiras <ewertoncarreira@gmail.com>
2023-08-10 12:01:06 -0300

    SRP - Single Responsability Principle - Melhorando a coesão

---
+++ src/rh/service/ReajusteService.java
@@ -0,0 +1,21 @@
+package rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+import rh.Funcionario;
+import rh.exception.ValidacaoException;
+
+public class ReajusteService {
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.atualizarSalario(salarioReajustado);
+    }
+}
