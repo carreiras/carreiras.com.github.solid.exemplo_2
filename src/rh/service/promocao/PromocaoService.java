7d7d7fe55b7407e63e591b8624b137122d8f2251
carreiras <ewertoncarreira@gmail.com>
2023-08-10 14:04:42 -0300

    LSP - Liskov Substitution Principle - Implementando uma nova regra de negócio

---
+++ src/rh/service/promocao/PromocaoService.java
@@ -0,0 +1,23 @@
+package rh.service.promocao;
+
+import rh.Cargo;
+import rh.Funcionario;
+import rh.exception.ValidacaoException;
+
+public class PromocaoService {
+
+    public void promover(Funcionario funcionario, Boolean metaBatida) {
+        Cargo cargoAtual = funcionario.getCargo();
+
+        if (Cargo.GERENTE == cargoAtual) {
+            throw new ValidacaoException("Gerentes não podem ser promovidos!");
+        }
+
+        if (metaBatida) {
+            Cargo novoCargo = cargoAtual.getProximoCargo();
+            funcionario.promover(novoCargo);
+        } else {
+            throw new ValidacaoException("Funcionário não bateu a meta!");
+        }
+    }
+}
